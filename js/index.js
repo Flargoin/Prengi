716bbe6b4243a905c765a190a2703ca9e11317ed
Sergey <Flargoin@gmail.com>
2021-11-07 22:03:57 +0300

    add modal

--- js/index.js
+++ js/index.js
@@ -1,4 +1,20 @@
-    $(document).ready(function(){
+$('.main__top-burger').on('click', function(e){
+  e.preventDefault();
+  $('.main__top-burger').toggleClass('burger__btn-elem-active');
+  $('.main__mobile-nav').toggleClass('main__mobile-nav_active');
+});
+
+$('.main__info-btn, .callback-btn').on('click', function(e){
+  e.preventDefault();
+  $('#modal, .overlay').fadeIn('slow');
+});
+$('.modal__close').on('click', function() {
+  $('#modal, .overlay').fadeOut('slow');
+});
+
+
+  
+  $(document).ready(function(){
   $('.slider__inner').slick({
     prevArrow: '<button type="button" class="slick-prev"><img src="icons/slider/left.png"></button>',
     nextArrow: '<button type="button" class="slick-next"><img src="icons/slider/right.png"></button>',
@@ -9,10 +25,17 @@
                 dots: true,
                 arrows: false
             }
-        }
+        },
+        {
+          breakpoint: 320,
+          settings: {
+            dots: true,
+            arrows: false
+          }
+        },
     ]
 });
-  });
+});
 
 
 
